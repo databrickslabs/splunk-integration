389eaeb96772ad561f8693378e6d62700ed6b2f1
Chris J <crattis@users.noreply.github.com>
2021-10-13 19:31:28 -0400

    Update DNS_Analytics.py to add ML cluster requirement

--- notebooks/source/DNS_Analytics.py
+++ notebooks/source/DNS_Analytics.py
@@ -2,6 +2,9 @@
 # MAGIC %md
 # MAGIC This is a companion notebook to <a href ="https://databricks.com/blog/2020/10/05/detecting-criminals-and-nation-states-through-dns-analytics.html"> Detecting Criminals and Nation States through DNS Analytics </a> blog.  
 # MAGIC We invite your feedback at cybersecurity@databricks.com .
+# MAGIC
+# MAGIC ### Requirements
+# MAGIC - Cluster running Databricks Runtime 7.5 ML or above
 
 # COMMAND ----------
 
