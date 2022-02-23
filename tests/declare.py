934672cb143c2f44a749a7951785bb2e62e71575
Keerthana Karthik <keerthana.karthik@crestdatasys.com>
2022-02-23 15:00:19 +0530

    Doc updates
    Added user guide and updated their links in the Readme.md file. Added the correct path in the tests\.coveragerc file

--- tests/declare.py
+++ tests/declare.py
@@ -1,4 +1,4 @@
 import os
 import sys
 
-sys.path.insert(0, os.path.abspath(os.path.join(__file__, "..", "..", "TA-Databricks", "bin")))
+sys.path.insert(0, os.path.abspath(os.path.join(__file__, "..", "..", "app", "bin")))
