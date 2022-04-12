b16b4935391f072197cf4c6fb760070969a67093
Keerthana Karthik <keerthana.karthik@crestdatasys.com>
2022-04-12 19:46:36 +0530

    Removed jquery3.5 packed inside app

--- app/appserver/static/js/handle_autoforward.js
+++ app/appserver/static/js/handle_autoforward.js
@@ -1,69 +1,59 @@
-require(["splunkjs/mvc/utils"], function (SplunkUtils) {
-    var app_name = "TA-Databricks"
-    require.config({
-        paths: {
-            'jquery_databricks': '../app/' + app_name + '/js/jquery_databricks',
-        },
-    });
-    require([
-        'jquery_databricks',
-        'splunkjs/mvc',
-        'splunkjs/mvc/simplexml/ready!',
-    ], function($,mvc) {
-        var submitted_model = mvc.Components.getInstance("submitted")
+require([
+    'splunkjs/mvc',
+    'splunkjs/mvc/simplexml/ready!',
+], function(mvc) {
+    var submitted_model = mvc.Components.getInstance("submitted")
 
-        function redirect_to_result_url(){
-            var autoforward = submitted_model.get("autoforward")
-            var url = submitted_model.get("url",null)
-            if (url && autoforward == "Yes") {
-                window.open(url, "_blank");
-            }
+    const retry_button = document.getElementById("retryButton")
+
+    function rerun() {
+        mvc.Components.get("notebookrun").startSearch()
+    }
+
+    retry_button.addEventListener('click', rerun, false);
+
+    function redirect_to_result_url(){
+        var autoforward = submitted_model.get("autoforward")
+        var url = submitted_model.get("url",null)
+        if (url && autoforward == "Yes") {
+            window.open(url, "_blank");
         }
-        function token_handling(param_name){
-            var param_value = submitted_model.get(param_name,null)
-            var redirected = submitted_model.get("redirected",null)
-            if (param_value == "True"){
-                submitted_model.unset("url")
-                submitted_model.unset("redirected")
-            }
-            else if(redirected == "True"){
-                submitted_model.set(param_name, "True")
-            }
-            else{
-                submitted_model.unset("url")
-            }
+    }
+    function token_handling(param_name){
+        var param_value = submitted_model.get(param_name,null)
+        var redirected = submitted_model.get("redirected",null)
+        if (param_value == "True"){
+            submitted_model.unset("url")
+            submitted_model.unset("redirected")
         }
+        else if(redirected == "True"){
+            submitted_model.set(param_name, "True")
+        }
+        else{
+            submitted_model.unset("url")
+        }
+    }
 
-        submitted_model.on("change:notebook", function() {
-            token_handling("notebook_changed")
-        })
-
-        submitted_model.on("change:revision_timestamp", function() {
-            token_handling("revision_changed")
-        })
-
-        submitted_model.on("change:params", function() {
-            token_handling("params_changed")
-        })
-
-        submitted_model.on("change:cluster", function() {
-            token_handling("cluster_changed")
-        })
-
-        submitted_model.on("change:url", function() {
-            redirect_to_result_url()
-        })
-        submitted_model.on("change:autoforward", function() {
-            redirect_to_result_url()
-        })
-        submitted_model.on("change:failedJob", function() {
-            var is_failed = submitted_model.get("failedJob",null)
-            if (is_failed) {
-                $("#retryButton").click(function() {
-                    mvc.Components.get("notebookrun").startSearch()
-                })
-            }
-        })
+    submitted_model.on("change:notebook", function() {
+        token_handling("notebook_changed")
     })
-        
+
+    submitted_model.on("change:revision_timestamp", function() {
+        token_handling("revision_changed")
+    })
+
+    submitted_model.on("change:params", function() {
+        token_handling("params_changed")
+    })
+
+    submitted_model.on("change:cluster", function() {
+        token_handling("cluster_changed")
+    })
+
+    submitted_model.on("change:url", function() {
+        redirect_to_result_url()
+    })
+    submitted_model.on("change:autoforward", function() {
+        redirect_to_result_url()
+    })
 });
--- app/appserver/static/js/handle_autoforward_old.js
+++ app/appserver/static/js/handle_autoforward_old.js
@@ -1,54 +1,38 @@
 require([
-     "splunkjs/mvc",
-     ], function(mvc) {
-    var app_name = "TA-Databricks"
- mvc.setFilter("dbquote", function(inputValue) {
-     return inputValue.replace(/"/g, '\\"').replace(/'/g, "\\'");
-   });
-   require.config({
-    paths: {
-        'jquery_databricks': '../app/' + app_name + '/js/jquery_databricks',
-        'underscore_utils': '../app/' +  app_name + '/js/underscore-min'
-        },
-    });
- });
-
-
-
+    "splunkjs/mvc",
+    ], function(mvc) {
+mvc.setFilter("dbquote", function(inputValue) {
+    return inputValue.replace(/"/g, '\\"').replace(/'/g, "\\'");
+  });
+});
 
 require(
-    [
-        'jquery_databricks',
-        'underscore_utils',
-        'backbone',
-        "splunk.util"
-    ],
-    function(
-        $,
-        _,
-        Backbone,
-        splunkUtil
-    ) {
-        $("#retryButton").click(function(){
-            splunkjs.mvc.Components.getInstance("notebookrun").startSearch()
-        })
-                
-        splunkjs.mvc.Components.getInstance("submitted").on("change", function(changeEvent) {
-            //console.log("Got a token change", changeEvent)
-            if (typeof changeEvent.changed.url != "undefined") {
-                //console.log("Got a change of the URL", changeEvent.changed.url)
-                if(splunkjs.mvc.Components.getInstance("submitted").toJSON()['autoforward'] && splunkjs.mvc.Components.getInstance("submitted").toJSON()['autoforward'] == "Yes"){
-                    //console.log("Redirecting to ", changeEvent.changed.url)
-                    window.location.href = changeEvent.changed.url
-                }
-            }
-            if (typeof changeEvent.changed.autoforward != "undefined") {
-                //console.log("Got a change of the Token Forwarding", changeEvent.changed.autoforward)
-                if(splunkjs.mvc.Components.getInstance("submitted").toJSON()['url'] && splunkjs.mvc.Components.getInstance("submitted").toJSON()['url'] != ""){
-                  //  console.log("Redirecting to ", changeEvent.changed.url)
-                    window.location.href = changeEvent.changed.url
-                }
-            }
-        })
+   [], function()
+   {
+       const retry_button = document.getElementById("retryButton")
 
-    })
+       function rerun() {
+           mvc.Components.get("notebookrun").startSearch()
+       }
+
+       retry_button.addEventListener('click', rerun, false);
+
+       splunkjs.mvc.Components.getInstance("submitted").on("change", function(changeEvent) {
+           //console.log("Got a token change", changeEvent)
+           if (typeof changeEvent.changed.url != "undefined") {
+               //console.log("Got a change of the URL", changeEvent.changed.url)
+               if(splunkjs.mvc.Components.getInstance("submitted").toJSON()['autoforward'] && splunkjs.mvc.Components.getInstance("submitted").toJSON()['autoforward'] == "Yes"){
+                   //console.log("Redirecting to ", changeEvent.changed.url)
+                   window.location.href = changeEvent.changed.url
+               }
+           }
+           if (typeof changeEvent.changed.autoforward != "undefined") {
+               //console.log("Got a change of the Token Forwarding", changeEvent.changed.autoforward)
+               if(splunkjs.mvc.Components.getInstance("submitted").toJSON()['url'] && splunkjs.mvc.Components.getInstance("submitted").toJSON()['url'] != ""){
+                 //  console.log("Redirecting to ", changeEvent.changed.url)
+                   window.location.href = changeEvent.changed.url
+               }
+           }
+       })
+
+   })
